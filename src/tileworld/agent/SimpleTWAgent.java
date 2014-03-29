e10e101f8dddd60c3b4d85b90efcb36ace6d7360
Sahil Bajaj <sahil29@gmail.com>
2014-03-29 15:42:09 +0800

    Start thinking properly

--- src/tileworld/agent/SimpleTWAgent.java
+++ src/tileworld/agent/SimpleTWAgent.java
@@ -6,10 +6,15 @@
 package tileworld.agent;
 
 import sim.field.grid.ObjectGrid2D;
+import tileworld.Parameters;
 import tileworld.environment.*;
 import tileworld.exceptions.CellBlockedException;
 import tileworld.planners.AstarPathGenerator;
 import tileworld.planners.TWPath;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TWContextBuilder
  *
@@ -28,7 +33,35 @@ public class SimpleTWAgent extends TWAgent{
         super(xpos,ypos,env,fuelLevel);
     }
 
-    protected TWThought think() {
+    List<TWEntity> getEntitiesInRange() {
+        int sensorRange = Parameters.defaultSensorRange;
+        List<TWEntity> entityList = new ArrayList<TWEntity>();
+
+        ObjectGrid2D objectGrid = this.getEnvironment().getObjectGrid();
+
+        int topLeftX = (this.getX() - sensorRange) < 0 ? 0 : this.getX() - sensorRange;
+        int topLeftY = (this.getY() - sensorRange) < 0 ? 0 : this.getY() - sensorRange;
+        int bottomRightX = (this.getX() + sensorRange) > Parameters.xDimension ? Parameters.xDimension : this.getX() + sensorRange;
+        int bottomRightY = (this.getY() + sensorRange) > Parameters.yDimension ? Parameters.yDimension : this.getY() + sensorRange;
+
+        for (int i = topLeftX; i <= bottomRightX; i++) {
+            for (int j = topLeftY; j <= bottomRightY; j++) {
+                TWEntity e = (TWEntity) objectGrid.get(i, j);
+
+                if (e == null) {
+                    continue;
+                }
+
+                else if (e instanceof TWTile || e instanceof TWHole) {
+                    entityList.add(e);
+                }
+            }
+        }
+
+        return entityList;
+    }
+
+    /*protected TWThought think() {
 //        getMemory().getClosestObjectInSensorRange(Tile.class);
         //DefaultTWPlanner plan = new DefaultTWPlanner();
         //plan.generatePlan();
@@ -55,6 +88,69 @@ public class SimpleTWAgent extends TWAgent{
             else return new TWThought(TWAction.MOVE,getRandomDirection());
 
         }
+    }*/
+
+    protected TWThought think() {
+        List<TWEntity> entityList = this.getEntitiesInRange();
+
+        if (entityList.size() > 0) {
+
+            // Find astar path to closest hole if we have carried tiles
+            if (this.hasTile()) {
+                double minDistance = Double.MAX_VALUE;
+                TWHole nearestHole = null;
+
+                for (TWEntity entity : entityList) {
+                    if (entity instanceof TWHole) {
+                        double distance = entity.getDistanceTo(this);
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            nearestHole = (TWHole) entity;
+                        }
+                    }
+                }
+
+                if (nearestHole != null) {
+                    AstarPathGenerator astar = new AstarPathGenerator(getEnvironment(), this, Parameters.defaultSensorRange * Parameters.defaultSensorRange);
+
+                    TWPath path = astar.findPath(x, y, nearestHole.getX(), nearestHole.getY());
+
+                    if (path != null) {
+                        return new TWThought(TWAction.MOVE, path.getStep(0).getDirection());
+                    }
+                }
+            }
+
+            // Otherwise find astar path to closest tile
+            if (this.carriedTiles.size() < 3) {
+                double minDistance = Double.MAX_VALUE;
+                TWTile nearestTile = null;
+
+                for (TWEntity entity : entityList) {
+                    if (entity instanceof TWTile) {
+                        double distance = entity.getDistanceTo(this);
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            nearestTile = (TWTile) entity;
+                        }
+                    }
+                }
+
+                if (nearestTile != null) {
+                    AstarPathGenerator astar = new AstarPathGenerator(getEnvironment(), this, Parameters.defaultSensorRange * Parameters.defaultSensorRange);
+
+                    TWPath path = astar.findPath(x, y, nearestTile.getX(), nearestTile.getY());
+
+                    if (path != null) {
+                        return new TWThought(TWAction.MOVE, path.getStep(0).getDirection());
+                    }
+                }
+            }
+
+        }
+
+        // Otherwise move randomly till you see something interesting
+        return new TWThought(TWAction.MOVE, getRandomDirection());
     }
 
     @Override
