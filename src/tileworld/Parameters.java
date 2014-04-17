4c08152a796b996de46ba171a8a04ea5bfef0fbf
Nishanth Chowdhary <nishanthchowdhary@hotmail.com>
2014-04-18 00:14:34 +0800

    FINAL!!!!!!!!!!!

--- src/tileworld/Parameters.java
+++ src/tileworld/Parameters.java
@@ -17,7 +17,7 @@ package tileworld;
 public class Parameters {
 
     //Simulation Parameters
-    //public final static int seed = 4162012; //no effect with gui
+    public final static int seed = 4162012; //no effect with gui
     public static final long endTime = 5000; //no effect with gui
 
     //Agent Parameters
@@ -40,31 +40,31 @@ public class Parameters {
 
 //****************************************************************
     //Environment Parameters II
-//    public static final int xDimension = 50; //size in cells
-//    public static final int yDimension = 50;
-//
-//    //Object Parameters
-//    public static final double tileMean = 2;
-//    public static final double holeMean = 2;
-//    public static final double obstacleMean = 2;
-//    public static final double tileDev = 0.5f;
-//    public static final double holeDev = 0.5f;
-//    public static final double obstacleDev = 0.5f;
-//    public static final int lifeTime = 30;
+    public static final int xDimension = 50; //size in cells
+    public static final int yDimension = 50;
+
+    //Object Parameters
+    public static final double tileMean = 2;
+    public static final double holeMean = 2;
+    public static final double obstacleMean = 2;
+    public static final double tileDev = 0.5f;
+    public static final double holeDev = 0.5f;
+    public static final double obstacleDev = 0.5f;
+    public static final int lifeTime = 30;
 
 //****************************************************************
 
 //    //Environment Parameters III
-    public final static int seed = 9042014;
-    public static final int xDimension = 150; //size in cells
-    public static final int yDimension = 40;
-
-    //Object Parameters
-    public static final double tileMean = 0.02;
-    public static final double holeMean = 0.2;
-    public static final double obstacleMean = 0.5;
-    public static final double tileDev = 0.001f;
-    public static final double holeDev = 0.01f;
-    public static final double obstacleDev = 0.1f;
-    public static final int lifeTime = 120;
+//    public final static int seed = 9042014;
+//    public static final int xDimension = 150; //size in cells
+//    public static final int yDimension = 40;
+//
+//    //Object Parameters
+//    public static final double tileMean = 0.02;
+//    public static final double holeMean = 0.2;
+//    public static final double obstacleMean = 0.5;
+//    public static final double tileDev = 0.001f;
+//    public static final double holeDev = 0.01f;
+//    public static final double obstacleDev = 0.1f;
+//    public static final int lifeTime = 120;
 }
